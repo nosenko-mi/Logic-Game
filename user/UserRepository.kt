47f1a9f978dbef0a896652a8be4ed85ee4e791c2
nickolay <nosenkomi02@gmail.com>
2022-11-09 19:49:01 +0200

    refactor

--- user/UserRepository.kt
+++ user/UserRepository.kt
@@ -28,76 +28,15 @@ class UserRepository {
     fun updateRecord(user: UserModel){
         val docRef = db.collection("users").document(user.email)
         docRef.set(user)
+            .addOnSuccessListener {
+                Log.d(TAG, "Document is updated")
+            }
+            .addOnFailureListener{
+                Log.d(TAG, "Document was not updated")
+            }
     }
 
-    fun findByEmail(email: String): UserModel?{
-/*        val docRef = db.collection("users").document(email)
-        var user: UserModel? = null
-        docRef.get()
-            .addOnSuccessListener { document ->
-                if (document.exists()){
-                    user = UserModel(
-                        document["displayname"].toString(),
-                        document["email"].toString(),
-                        document["record"] as Long
-                    )
-                    Log.d(TAG, "Document: ${document.id}")
-                } else {
-                    Log.d(TAG, "Document not exists: ${document.id}")
-                }
-            }
-            .addOnFailureListener { exception ->
-                Log.d(TAG, "get failed with ", exception)
-            }
-            .addOnCompleteListener {
-            Log.d(TAG, "user: " + user.toString())
-            }
-        return user*/
-
-        val docRef = db.collection("users").document(email)
-        var user: UserModel? = null
-        docRef.get()
-            .addOnSuccessListener { document ->
-                if (document.exists()){
-                    user = UserModel(
-                        document["displayname"].toString(),
-                        document["email"].toString(),
-                        document["record"] as Long
-                    )
-                    Log.d(TAG, "Document: ${document.id}")
-                    Log.d(TAG, "user: " + user.toString())
-
-                } else { Log.d(TAG, "Document not exists: ${document.id}") }
-            }
-            .addOnFailureListener { exception ->
-                Log.d(TAG, "get failed with ", exception)
-            }
-        return user
-
-/*        var user: UserModel? = null
-        db.collection("users")
-            .whereEqualTo("email", email)
-            .get()
-            .addOnSuccessListener { documents ->
-                for (document in documents) {
-                    Log.d(TAG, "Document: ${document.id}")
-                    user = UserModel(
-                        document["displayname"].toString(),
-                        document["email"].toString(),
-                        document["record"] as Long
-                    )
-                }
-            }
-            .addOnFailureListener { exception ->
-                Log.w(TAG, "Error getting documents: ", exception)
-            }
-            .addOnCompleteListener {
-                Log.d(TAG, "user: " + user.toString())
-            }
-        return user*/
-    }
-
-    fun findByEmail1(email: String, model: UserViewModel){
+    fun findByEmail(email: String, model: UserViewModel){
         val docRef = db.collection("users").document(email)
         var user: UserModel?
 
